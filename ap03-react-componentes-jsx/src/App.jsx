015c3a35bbe7f711bf22d25b6834f4a60ced59d1
Rodrigo Bossini <professorbossini@gmail.com>
2025-08-28 20:41:02 -0300

    bateria

--- ap03-react-componentes-jsx/src/App.jsx
+++ ap03-react-componentes-jsx/src/App.jsx
@@ -1,7 +1,13 @@
 const App = () => {
   return(
-    <div style={{margin: 'auto', width: 768}}>
 
+    <div style={{margin: 'auto', width: 768, backgroundColor: '#EEE', padding: 12, borderRadius: 8}}>
+      <label 
+        htmlFor="nome"
+        style={{display: 'block', marginBottom: 4}}>
+        Nome:
+      </label>
+      <input type="text" />
     </div> 
   )
 }
