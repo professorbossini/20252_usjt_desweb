ba6d91d26fcdf0159ac6996bd7dd92fb56e879bd
Rodrigo Bossini <professorbossini@gmail.com>
2025-09-18 21:57:06 -0300

    ap04(react): encerra a apostila

--- ap04-react-props/src/App.jsx
+++ ap04-react-props/src/App.jsx
@@ -1,64 +1,90 @@
 import Pedido from "./Pedido"
 import Animal from "./Animal"
 import Cartao from "./Cartao"
+import Feedback from "./Feedback"
 const App = () => {
+  const funcaoOK = () => alert('Agradecemos o feedback')
+  const funcaoNOK = () => alert('Verificaremos')
+  const textoOK = 'Pedido já chegou'
+  const textoNOK = 'Ainda não chegou'
+  const pedidos = [
+    {
+      data: '15/09/2025',
+      icone: 'car',
+      titulo: 'Carro',
+      descricao: 'Porsche Amarelo'
+    },
+    {
+      data: '12/09/2025',
+      icone: 'house',
+      titulo: 'Casa',
+      descricao: 'Casa Amarela'
+    }
+  ]
+  const expressaoFeedback = (
+    <Feedback 
+      funcaoOK={funcaoOK}
+      funcaoNOK={funcaoNOK}
+      textoOK={textoOK}
+      textoNOK={textoNOK}/>
+  )
   return (
     <div className="container-fluid">
       <div className="row">
         <div className="col-12">
           <Cartao
             cabecalho="Família de animais peculiar">
-            <Animal
-              tipo="cat"
-              tamanho="3" />
-            <Animal
-              tipo="cat"
-              tamanho="2"
-              espelhamento="flip-horizontal" />
-            <Animal
-              tipo="cat"
-              tamanho="1"
-              espelhamento="flip-horizontal" />
-            <Animal
-              tipo="cat"
-              tamanho="1"
-              espelhamento="flip-horizontal" />
-            <Animal
-              tipo="hippo"
-              tamanho="1"
-              espelhamento="flip-horizontal" />
+              <div className="d-flex flex-column">
+                <div 
+                  className="d-flex justify-content-center align-items-end mb-5">
+                  <Animal
+                    tipo="cat"
+                    tamanho="3" />
+                  <Animal
+                    tipo="cat"
+                    tamanho="2"
+                    espelhamento="flip-horizontal" />
+                  <Animal
+                    tipo="cat"
+                    tamanho="1"
+                    espelhamento="flip-horizontal" />
+                  <Animal
+                    tipo="cat"
+                    tamanho="1"
+                    espelhamento="flip-horizontal" />
+                  <Animal
+                    tipo="hippo"
+                    tamanho="1"
+                    espelhamento="flip-horizontal" />
+                </div>
+                <Feedback 
+                  textoOK="Adotar uma lontra?"
+                  textoNOK="Não adotar!"
+                  funcaoOK={() => alert('Oba!')}
+                  funcaoNOK={() => alert('Ahhh')}/>
+              </div>
           </Cartao>
         </div>
       </div>
-      <div className="row">
-        <div className="col-sm-12 col-md-6 col-xl-3">
-          <Pedido
-            data="14/09/2025"
-            icone="car"
-            titulo="Carro"
-            descricao="Porsche Amarelo" />
-        </div>
-        <div className="col-sm-12 col-md-6 col-lg-3">
-          <Pedido
-            data="12/09/2025"
-            icone="tractor"
-            titulo="Trator"
-            descricao="Trator Amarelo" />
-        </div>
-        <div className="col-sm-12 col-md-6 col-lg-3">
-          <Pedido
-            data="12/09/2025"
-            icone="house"
-            titulo="Casa"
-            descricao="Casa Amarela" />
-        </div>
-        <div className="col-sm-12 col-md-6 col-lg-3">
-          <Pedido
-            data="12/09/2025"
-            icone="computer"
-            titulo="Computador"
-            descricao="Computador Amarelo" />
-        </div>
+      <div className="row mt-2">
+        {
+          pedidos.map(({data, icone, titulo, descricao}) =>{
+            return(
+              <div className="col-sm-12 col-md-6 col-xl-4">
+                <Cartao
+                  cabecalho={data}>
+                  <Pedido
+                    icone={icone}
+                    titulo={titulo}
+                    descricao={descricao} />
+                  <div className="mt-3"></div>
+                  {expressaoFeedback}
+                </Cartao>
+              </div>
+            )
+          })
+        }
+        
       </div>
 
     </div>
